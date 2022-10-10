a986ab5340f1f214dc482531536190e3f33fc01d
username <phil042017@gmail.com>
2022-10-10 16:28:16 -0400

    Initial Commit

---
+++ GitHub Projects/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GitHub Projects
+//
+//  Created by Philippe Mathieu on 10/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
